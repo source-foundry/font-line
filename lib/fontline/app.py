3145ec1f9d26862b3232f7b52c3584854326cb8d
Chris Simpkins <git.simpkins@gmail.com>
2016-03-23 17:14:17 -0400

    help and usage support added

--- lib/fontline/app.py
+++ lib/fontline/app.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+
 from commandlines import Command
-from fontlines import settings
+from fontline import settings
 from standardstreams import stdout, stderr
 
 
 def main():
     c = Command()
 
+    if c.does_not_validate_missing_args():
+        stderr("[font-line] ERROR: please include one or more arguments with your command.\n")
+        sys.exit(1)
+
     if c.is_help_request():
         stdout(settings.HELP)
+        sys.exit(0)
+    elif c.is_version_request():
+        stdout(settings.VERSION)
+        sys.exit(0)
+    elif c.is_usage_request():
+        pass
 
-
-if __name__ == '__main__':
-    main()
