55f7445cee8a7ec1fbb4b8af95debaeee0a07a85
Chris Simpkins <git.simpkins@gmail.com>
2016-03-26 11:02:42 -0400

    added CI testing, new test of stdout/stderr stream capture via pytest

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,3 +3,21 @@
 
 import sys
 import pytest
+
+
+# ///////////////////////////////////////////////////////
+#
+# pytest capsys capture tests
+#    confirms capture of std output and std error streams
+#
+# ///////////////////////////////////////////////////////
+
+def test_pytest_capsys(capsys):
+    print("bogus text for a test")
+    sys.stderr.write("more text for a test")
+    out, err = capsys.readouterr()
+    assert out == "bogus text for a test\n"
+    assert out != "something else"
+    assert err == "more text for a test"
+    assert err != "something else"
+
