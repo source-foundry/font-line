eb8396e0ea2d8284900e33911482f804c98bf101
Chris Simpkins <git.simpkins@gmail.com>
2016-03-26 15:24:31 -0400

    new report command tests

--- tests/test_report_cmd.py
+++ tests/test_report_cmd.py
@@ -4,6 +4,8 @@
 import sys
 import pytest
 import os.path
+import os
+from fontline.utilities import file_exists
 
 # ///////////////////////////////////////////////////////
 #
@@ -31,9 +33,53 @@ def test_report_cmd_missing_file_request(capsys):
 
 def test_report_cmd_unsupported_filetype(capsys):
     from fontline.app import main
-    filepath = os.path.join('testingfiles', 'missing.txt')
+    filepath = os.path.join('tests', 'testingfiles', 'bogus.txt')
     sys.argv = ['font-line', 'report', filepath]
     main()
     out, err = capsys.readouterr()
     assert err.startswith("[font-line] ERROR: ")
 
+
+def test_report_cmd_reportstring_filename(capsys):
+    from fontline.app import main
+    filepath = os.path.join('tests', 'testingfiles', 'Hack-Regular.ttf')
+    sys.argv = ['font-line', 'report', filepath]
+    main()
+    out, err = capsys.readouterr()
+    assert "Hack-Regular.ttf " in out
+
+
+def test_report_cmd_reportstring_version(capsys):
+    from fontline.app import main
+    filepath = os.path.join('tests', 'testingfiles', 'Hack-Regular.ttf')
+    sys.argv = ['font-line', 'report', filepath]
+    main()
+    out, err = capsys.readouterr()
+    assert "Version 2.020;DEV-03192016;" in out
+
+
+def test_report_cmd_reportstring_sha1(capsys):
+    from fontline.app import main
+    filepath = os.path.join('tests', 'testingfiles', 'Hack-Regular.ttf')
+    sys.argv = ['font-line', 'report', filepath]
+    main()
+    out, err = capsys.readouterr()
+    assert "SHA1: 638f033cc1b6a21597359278bee62cf7e96557ff" in out
+
+
+def test_report_cmd_reportstring_upm(capsys):
+    from fontline.app import main
+    filepath = os.path.join('tests', 'testingfiles', 'Hack-Regular.ttf')
+    sys.argv = ['font-line', 'report', filepath]
+    main()
+    out, err = capsys.readouterr()
+    assert "[head] Units per Em:    2048" in out
+
+
+def test_report_cmd_reportstring_typoascender(capsys):
+    from fontline.app import main
+    filepath = os.path.join('tests', 'testingfiles', 'Hack-Regular.ttf')
+    sys.argv = ['font-line', 'report', filepath]
+    main()
+    out, err = capsys.readouterr()
+    assert "[OS/2] TypoAscender:    1556" in out
