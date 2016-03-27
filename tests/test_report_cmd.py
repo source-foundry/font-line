75d330a0b7a83cc687174a6014f1358c6d5c8776
Chris Simpkins <git.simpkins@gmail.com>
2016-03-26 23:33:53 -0400

    corrected another error in the test string for report tests

--- tests/test_report_cmd.py
+++ tests/test_report_cmd.py
@@ -144,7 +144,7 @@ def test_report_cmd_reportstring_typolinegap(capsys):
     sys.argv = ['font-line', 'report', filepath]
     main()
     out, err = capsys.readouterr()
-    assert "[hhea] TypoLineGap: \t410" in out
+    assert "[OS/2] TypoLineGap: \t410" in out
 
 
 def test_report_cmd_reportstring_typoA_typoD(capsys):
