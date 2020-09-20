47f072133548674f9ba9f4c1863a929b0b0c88b3
Chris Simpkins <git.simpkins@gmail.com>
2020-09-20 14:49:53 -0400

    [app.py] adjust line length to < 90

--- lib/fontline/app.py
+++ lib/fontline/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 """
-The app.py module defines a main() function that includes the logic for the `font-line` command line executable.
+The app.py module defines a main() function that includes the logic for the `font-line`
+command line executable.
 
-This command line executable modifies the line spacing metrics in one or more fonts that are provided as command
+This command line executable modifies the line spacing metrics in one or more fonts that
+are provided as command
 line arguments to the executable.  It currently supports .ttf and .otf font builds.
 """
 
@@ -19,9 +21,6 @@ from fontline.commands import (
     get_linegap_percent_filepath,
 )
 from fontline.utilities import file_exists, is_supported_filetype
-
-# TODO: support integer addition and subtraction to the metrics values through new sub-commands
-# TODO: JSON formatted metrics output
 
 
 def main():
@@ -48,7 +47,8 @@ def main():
     if c.subcmd == "report":
         if c.argc < 2:
             stderr(
-                "[font-line] ERROR: Missing file path argument(s) after the report subcommand."
+                "[font-line] ERROR: Missing file path argument(s) after the "
+                "report subcommand."
             )
             sys.exit(1)
         else:
@@ -84,13 +84,14 @@ def main():
                 )  # test that the argument can be cast to an integer value
                 if percent_int <= 0:
                     stderr(
-                        "[font-line] ERROR: Please enter a percent value that is greater than zero."
+                        "[font-line] ERROR: Please enter a percent value that is "
+                        "greater than zero."
                     )
                     sys.exit(1)
                 if percent_int > 100:
                     stdout(
-                        "[font-line] Warning: You entered a percent value over 100%. Please confirm that this is "
-                        "your intended metrics modification."
+                        "[font-line] Warning: You entered a percent value over 100%. "
+                        "Please confirm that this is your intended metrics modification."
                     )
             except ValueError:
                 stderr(
@@ -131,7 +132,7 @@ def main():
                     )
     else:
         stderr(
-            "[font-lines] ERROR: You used an unsupported argument to the executable. Please review the"
-            " `font-line --help` documentation and try again."
+            "[font-lines] ERROR: You used an unsupported argument to the executable. "
+            "Please review the `font-line --help` documentation and try again."
         )
         sys.exit(1)
