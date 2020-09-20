c149ffb4ac2af44abda952987ea344b643b5d9b6
Chris Simpkins <git.simpkins@gmail.com>
2020-09-20 14:55:18 -0400

    remove unnecessary scripts

--- coverage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-coverage run --source fontline -m py.test
-coverage report -m
-coverage html
-
-#coverage xml
-#codecov --token=$CODECOV_FONTLINE
--- release.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-python3 setup.py sdist bdist_wheel
-#twine upload dist/font-line-0.7.0*
